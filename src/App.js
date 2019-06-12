20a44e75f53c95aec702765b6b04498d02bc0449
weedking <260815998@qq.com>
2019-06-12 20:23:00 +0800

    saving before ejecting

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import Button from 'antd/lib/button';
+import { Button } from 'antd';
+// import logo from './logo.svg';
+import './App.css';
+import Qiandao from "./Qiandao";
+
+function App() {
+  return (
+    <div className="App">
+        <Qiandao/>
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Qiandao.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Button } from 'antd';
+
+
+class Qiandao extends Component {
+
+
+    render() {
+        return (
+            <Button type="primary">提交</Button>
+
+        );
+    }
+}
+
+export default Qiandao;
